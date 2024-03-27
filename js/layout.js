8307166744708c4522d0c4c52a24bc33f17b3e48
taedonn <taedonn@taedonn.com>
2024-03-27 16:35:08 +0900

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -58,13 +58,15 @@ document.addEventListener('DOMContentLoaded', () => {
         <div style="width:100%;">
             <div style="width: 100%; margin: 0 auto; padding: 2.5rem 1.75rem; box-sizing: border-box; background-color: #FFF; font-size: 1rem; font-weight: 400; text-align: center; line-height: 1.75; color: #333; font-family: 'Roboto', 'Noto Sans KR', '맑은고딕', Malgun Gothic, '돋움', Dotum, Helvetica, 'Apple SD Gothic Neo', Sans-serif;">
                 <div style="width: 100%; max-width: 30rem; margin: 0 auto; font-size: 1.25rem; font-weight: 700;">파티원님! 인원이 모두 모였어요</div>
-                <div style="width: 100%; max-width: 18.75rem; margin: 0 auto; margin-top: 0.75rem; padding-right: 2rem;">
+                <div style="width: 100%; max-width: 18.75rem; margin: 0 auto; margin-top: 0.75rem; padding-right: 2.25rem;">
                     <img src="https://partywonguham.s3.ap-northeast-2.amazonaws.com/3d_calendar.png" alt="3D 캘린더" style="width: 100%;"/>
                 </div>
                 <div style="width: 100%; max-width: 30rem; margin: 0 auto; margin-top: 0.25rem; font-size: 1rem;">
                     아래 버튼을 눌러 시간을 확인해 주세요.
                 </div>
-                <a href="https://partywon.vercel.app/p/${"aw3aafs3j"}" target="_blank" style="display: block; width: 100%; max-width: 20rem; height: 3rem; margin: 0 auto; margin-top: 1.25rem; line-height: 3rem; text-align: center; border-radius: 0.5rem; background-color: #FF6E40; color: #FFF; text-decoration: none;">링크 바로가기</a>
+                <div style="width: 100%; max-width: 20rem; margin: 0 auto;">
+                    <a href="https://partywon.vercel.app/p/${"aw3aafs3j"}" target="_blank" style="display: inline-block; width: 100%; padding: 0.625rem 5.5rem; margin: 0 auto; margin-top: 1.25rem; text-align: center; border-radius: 0.5rem; background-color: #FF6E40; color: #FFF; text-decoration: none;">링크 바로가기</a>
+                </div>
                 <div style="font-size: 0.875rem; margin-top: 2.5rem;">
                     <div>클릭이 안된다면? 아래 URL을 복사해 보세요.</div>
                     <a href="https://partywon.vercel.app/p/${"aw3aafs3j"}" target="_blank" style="color: #FF6E40;">https://partywon.vercel.app/p/${"aw3aafs3j"}</a>
